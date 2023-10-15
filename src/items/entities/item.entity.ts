03bacb8a225626f511aeec4f2460c5dd4d759ede
Julian Barbosa <julianbarbossa@hotmail.com>
2023-10-15 16:38:32 -0500

    add fields for us entity and create the type in graphql and typeorm

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,7 +1,27 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, ID, Float } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'items' })//With this we say this is a entity
 @ObjectType()
 export class Item {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  //columns 
+
+  @PrimaryGeneratedColumn('uuid')//typeorm
+  @Field(() => ID)//graphql
+  id: string
+
+  @Column()//typeorm
+  @Field(() => String) //graphql
+  name: string
+  
+  @Column()//typeorm
+  @Field(() => Float) //graphql
+  quantity: number
+  
+  @Column()//typeorm
+  @Field(() => String) //graphql
+  quantityUnits: string //gr, ml,kg, 
+
+  // stores
+  // users
 }
