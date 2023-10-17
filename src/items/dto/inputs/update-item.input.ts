ec4f9881f881d6da19bc891346c46db6f74e25a5
Julian Barbosa <julianbarbossa@hotmail.com>
2023-10-16 19:40:37 -0500

    create InpuType for update function and aslo update the type of the id

--- src/items/dto/inputs/update-item.input.ts
+++ src/items/dto/inputs/update-item.input.ts
@@ -1,8 +1,20 @@
+import { IsUUID } from 'class-validator';
 import { CreateItemInput } from './create-item.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { InputType, Field, Int, PartialType, ID } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateItemInput extends PartialType(CreateItemInput) {
-  @Field(() => Int)
-  id: number;
+  
+  @Field(() => ID)
+  @IsUUID()
+  id: string;
+
+  // @Field(() => String)
+  // name: string;
+
+  // @Field(() => Number)
+  // quantity: number;
+
+  // @Field(() => String, {nullable: true})
+  // quantityUnits?: string;
 }
