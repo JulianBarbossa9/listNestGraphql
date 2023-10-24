9e50ddc2e97e65dfbacc377d455b7c8c160d40bf
Julian Barbosa <julianbarbossa@hotmail.com>
2023-10-23 19:04:08 -0500

    add service login v1 and check if the password is correct

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
-import { SignUpInput } from './dto/inputs/signup.input';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { SignUpInput, LoginInput } from './dto/inputs/index';
 import { AuthResponse } from './types/auth-response.type';
 import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
@@ -15,5 +16,23 @@ export class AuthService {
     const token = '123';
 
     return { token, user };
+  }
+
+  async login(loginInput: LoginInput): Promise<AuthResponse> {
+    const { email, password } = loginInput //password come tho the input
+    const userLogin = await this.usersSevice.findOneByEmail(email)//db password hash
+    const token = 'ABC123'
+
+    //Compare password to come for input to password in db
+    if( !bcrypt.compareSync( password, userLogin.password )){
+      throw new BadRequestException('Email/Password is incorrect')
+    }
+
+    //TODO: JWT
+
+    return {
+      token,
+      user: userLogin
+    }
   }
 }
