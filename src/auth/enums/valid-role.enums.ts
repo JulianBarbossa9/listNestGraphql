796541769575b506937f6f6bdd2680d943642320
Julian Barbosa <julianbarbossa@hotmail.com>
2023-10-25 18:11:36 -0500

    add valid roles enum and also registerEnumType  and add description

--- src/auth/enums/valid-role.enums.ts
+++ src/auth/enums/valid-role.enums.ts
@@ -1,6 +1,12 @@
+import { registerEnumType } from "@nestjs/graphql";
 
 export enum ValidRoles {
   user        = 'user',
   admin       = 'admin',
   superUser   = 'superUser',
 }
+
+const props = Object.keys(ValidRoles)
+
+registerEnumType( ValidRoles, { name: 'ValidRoles', description:`This is are the only args allowed: ${props.join(', ')}`}, )//The second arg is the name that appear in graphql
+
