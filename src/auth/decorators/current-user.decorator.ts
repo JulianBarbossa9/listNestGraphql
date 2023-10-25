6be228ff3ed4a47413df4dfbf9988b1b653a19de
Julian Barbosa <julianbarbossa@hotmail.com>
2023-10-25 11:15:47 -0500

    update the type of request if you have a role

--- src/auth/decorators/current-user.decorator.ts
+++ src/auth/decorators/current-user.decorator.ts
@@ -1,17 +1,33 @@
-import { ExecutionContext, InternalServerErrorException, createParamDecorator } from "@nestjs/common";
+import { ExecutionContext, ForbiddenException, InternalServerErrorException, createParamDecorator } from "@nestjs/common";
 import { GqlExecutionContext } from "@nestjs/graphql";
+import { ValidRoles } from "../enums/valid-role.enums";
+import { User } from "src/users/entities/user.entity";
 
 export const CurrentUser = createParamDecorator( 
-  ( roles = [], context: ExecutionContext) => {
+  //With this we say this type of the roles that we have
+  ( roles : ValidRoles[] = [], context: ExecutionContext) => {
+    // console.log(roles)
     const ctx = GqlExecutionContext.create(context)
     //take user that comes to the request
-    const user = ctx.getContext().req.user //this can be null
+    const user: User = ctx.getContext().req.user //this can be null
 
     if(!user){
       //This is a error front back
       throw new InternalServerErrorException(`No user inside the request - make sure that we used the AuthGuard`)
     }
 
-    return user
+    //We say that if in the arg is empty, is open to everyone
+    if ( roles.length === 0) return user
+
+    //
+    for (const role of user.roles){
+      if ( roles.includes(role as ValidRoles)){ //we check in our array of 'roles' if the role in user.roles exist
+        return user
+      }
+    }
+    //If in the arg not come a role, then show this
+    throw new ForbiddenException(
+      `User ${user.fullName} need a valid role, the current role is: ${roles} `
+    )
 
 })
