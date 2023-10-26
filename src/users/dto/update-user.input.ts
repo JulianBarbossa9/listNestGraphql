787202f86151fe92e0575c0a4db61c3ab9f82e77
Julian Barbosa <julianbarbossa@hotmail.com>
2023-10-26 10:47:59 -0500

    update type of the id to string

--- src/users/dto/update-user.input.ts
+++ src/users/dto/update-user.input.ts
@@ -1,8 +1,24 @@
+import { IsArray, IsBoolean, IsNotEmpty, IsOptional } from 'class-validator';
 import { CreateUserInput } from './create-user.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { InputType, Field, Int, PartialType, ID } from '@nestjs/graphql';
+import { ValidRoles } from 'src/auth/enums/valid-role.enums';
 
 @InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
-  @Field(() => Int)
-  id: number;
+  
+  //The another field are 
+  @Field(() => ID)
+  @IsNotEmpty()
+  id: string;
+
+  @Field(() => [ValidRoles], { nullable: true})
+  @IsArray()
+  @IsOptional()
+  roles?: ValidRoles[]
+  
+  @Field(() => Boolean, { nullable: true})
+  @IsBoolean()
+  @IsOptional()
+  isActive?: boolean
+
 }
